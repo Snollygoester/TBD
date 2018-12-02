8d1ff5caa00a7bfebf7e492f96fec476a9c60b51
omer <snollygoester1133@gmail.com>
2018-12-02 22:02:15 +0200

    adding rotating eggs

--- Source/TBD/Characters/CluckTheChicken.cpp
+++ Source/TBD/Characters/CluckTheChicken.cpp
@@ -3,7 +3,17 @@
 #include "CluckTheChicken.h"
 #include "Engine/World.h"
 #include "SpecialActors/ShiledParent.h"
-
+#include "Components/StaticMeshComponent.h"
+ACluckTheChicken::ACluckTheChicken()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	EggF = CreateDefaultSubobject< UStaticMeshComponent>(FName("EggF"));
+	EggL = CreateDefaultSubobject< UStaticMeshComponent>(FName("EggL"));
+	EggI = CreateDefaultSubobject< UStaticMeshComponent>(FName("EggI"));
+	EggF->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+	EggL->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+	EggI->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+}
 void ACluckTheChicken::BlockDoYourThing() {
 	if (bIsShiledActive)
 	{
@@ -25,4 +35,68 @@ void ACluckTheChicken::BeginPlay()
 	 Shiled = GetWorld()->SpawnActor<AShiledParent>(ShiledParentTsubClass, FTransform(FRotator(0, 0, 0), GetActorLocation() + (GetActorForwardVector() * 100), FVector(1)));
 	Shiled->AttachToActor(this, FAttachmentTransformRules::KeepWorldTransform);
 	Shiled->Dactive();
+}
+
+void ACluckTheChicken::Tick(float DeltaTime)
+{
+	FVector NewLocationF = GetActorLocation();
+	AngleAxisF += DeltaTime * MultiplierF;
+
+	if (AngleAxisF >= 360.0f)
+	{
+		AngleAxisF = 0;
+		MultiplierF = 50;
+	}
+
+	FVector RotateValueF = Dimensions.RotateAngleAxis(AngleAxisF, AxisVector);
+
+	NewLocationF.X += RotateValueF.X;
+	NewLocationF.Y += RotateValueF.Y;
+	NewLocationF.Z += RotateValueF.Z;
+
+	FRotator NewRotationF = FRotator(0, AngleAxisF, 0);
+
+	FQuat QuatRotationF = FQuat(NewRotationF);
+	EggF->SetWorldLocationAndRotation(NewLocationF, QuatRotationF, false, 0, ETeleportType::None);
+	
+	FVector NewIocationL = GetActorLocation();
+	AngleAxisL += DeltaTime * MultiplierL;
+
+	if (AngleAxisL >=   360.0f)
+	{
+		AngleAxisL = 0;
+		MultiplierL = 50;
+	}
+
+	FVector RotateValueL = Dimensions.RotateAngleAxis(AngleAxisL, AxisVector);
+
+	NewIocationL.X += RotateValueL.X;
+	NewIocationL.Y += RotateValueL.Y;
+	NewIocationL.Z += RotateValueL.Z;
+
+	FRotator NewRotationL = FRotator(0, AngleAxisL, 0);
+
+	FQuat QuatRotationL = FQuat(NewRotationL);
+	EggL->SetWorldLocationAndRotation(NewIocationL, QuatRotationL, false, 0, ETeleportType::None);
+	
+	FVector NewIocationI = GetActorLocation();
+	AngleAxisI += DeltaTime * MultiplierI;
+
+	if (AngleAxisI >= 360.0f)
+	{
+		AngleAxisI = 0;
+		MultiplierI = 50;
+	}
+
+	FVector RotateValueI = Dimensions.RotateAngleAxis(AngleAxisI, AxisVector);
+
+	NewIocationI.X += RotateValueI.X;
+	NewIocationI.Y += RotateValueI.Y;
+	NewIocationI.Z += RotateValueI.Z;
+
+	FRotator NewRotationI = FRotator(0, AngleAxisI, 0);
+
+	FQuat QuatRotationI = FQuat(NewRotationI);
+	EggI->SetWorldLocationAndRotation(NewIocationI, QuatRotationI, false, 0, ETeleportType::None);
+	
 }
--- Source/TBD/Characters/CluckTheChicken.h
+++ Source/TBD/Characters/CluckTheChicken.h
@@ -13,7 +13,12 @@ UCLASS()
 class TBD_API ACluckTheChicken : public ACharacterParent
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this character's properties
+	ACluckTheChicken();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 private:
 	void BlockDoYourThing() override;
 	void BeginPlay() override;
@@ -22,4 +27,29 @@ private:
 	bool bIsShiledActive = false;
 	UPROPERTY()
 		class AShiledParent * Shiled;
+	UPROPERTY(VisibleAnywhere)
+		class UStaticMeshComponent * EggF;
+	UPROPERTY(VisibleAnywhere)
+		class UStaticMeshComponent * EggL;
+	UPROPERTY(VisibleAnywhere)
+		class UStaticMeshComponent * EggI;
+	UPROPERTY(EditAnywhere, Category = Movement)
+		float AngleAxisF;
+	UPROPERTY(EditAnywhere, Category = Movement)
+		float AngleAxisL;
+	UPROPERTY(EditAnywhere, Category = Movement)
+		float AngleAxisI;
+
+	UPROPERTY(EditAnywhere, Category = Movement)
+		FVector Dimensions = FVector(150,0,0);
+
+	UPROPERTY(EditAnywhere, Category = Movement)
+		FVector AxisVector = FVector(0,0,1) ;
+
+	UPROPERTY(EditAnywhere, Category = Movement)
+		float MultiplierF = 50;
+	UPROPERTY(EditAnywhere, Category = Movement)
+		float MultiplierL = 300;
+	UPROPERTY(EditAnywhere, Category = Movement)
+		float MultiplierI = 500;
 };
